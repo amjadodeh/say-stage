4740a20c16f044e863a0f5db93cb799475b0b8bd
amjad <aao001056485@gmail.com>
2021-05-30 05:01:37 -0400

    set up basic html and css, ready for js :)

---
+++ script.js
@@ -0,0 +1,9 @@
+const body = document.body;
+
+const div = document.querySelector('.subtitles');
+
+div.append('Test, ');
+div.append('test, ');
+div.append('test, ');
+div.append('test, ');
+div.append('and test. ');
