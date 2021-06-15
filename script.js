dba00619a4fa4ed0ce25e54ace636dfd3b2c1ce1
amjad <aao001056485@gmail.com>
2021-06-15 13:40:52 -0400

    proof of concept code

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const body = document.body;
 
 const div = document.querySelector('.subtitles');
+var video = document.querySelector('video');
+var source = document.querySelector('source');
 
-div.append('Test, ');
-div.append('test, ');
-div.append('test, ');
-div.append('test, ');
-div.append('and test. ');
+const exampleText = 'example';
+
+setInterval(() => {
+  if (div.innerText === '...') {
+    div.innerText = `${exampleText} `;
+  } else if (div.innerText.split(' ').length < 4) {
+    div.append(`${exampleText} `);
+  } else if (div.innerText.split(' ').length == 4) {
+    div.append(`${exampleText}`);
+  } else {
+    div.innerText = '...';
+  }
+}, 750);
+
+setInterval(() => {
+  if (source.src === 'http://127.0.0.1:5500/defaultBackgrounds/1.mp4') {
+    source.src = '/defaultBackgrounds/2.mp4';
+  } else if (source.src === 'http://127.0.0.1:5500/defaultBackgrounds/2.mp4') {
+    source.src = '/defaultBackgrounds/3.mp4';
+  } else if (source.src === 'http://127.0.0.1:5500/defaultBackgrounds/3.mp4') {
+    source.src = '/defaultBackgrounds/4.mp4';
+  } else {
+    source.src = '/defaultBackgrounds/1.mp4';
+  }
+
+  video.load();
+}, 20000);
